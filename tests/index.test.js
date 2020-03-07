0ba6cf3d7b191c86ec144d8772973fa74f34503b
Daniel Arthur Gallagher <8526031+DanArthurGallagher@users.noreply.github.com>
2020-03-07 16:59:10 +0100

    refactor: remove Jest The devDependencies are much lighter now. The test file runs in Mocha. The manifest is somewhat updated.
    test: refactor out Jest
    The tests were so simple that it made no sense to use it.
    
    chore: dedupe the lockfile

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,62 +1,66 @@
 'use strict';
 
+const assert = require('assert');
 const normalizeFileExtension = require('../index.js');
 
 describe('The normalization function', () => {
-    describe('validation', () =>{
-        it('throws when given a number', () => {
-            expect(() => normalizeFileExtension(5)).toThrow(TypeError)
-        })
+  describe('validation', () => {
+    it('throws when given a number', () => {
+      assert.throws(() => normalizeFileExtension(5), TypeError);
+    });
 
-        it('throws when given a boolean', () => {
-            expect(() => normalizeFileExtension(true)).toThrow(TypeError)
-            expect(() => normalizeFileExtension(false)).toThrow(TypeError)            
-        })
+    it('throws when given a boolean', () => {
+      assert.throws(() => normalizeFileExtension(true), TypeError);
+      assert.throws(() => normalizeFileExtension(false), TypeError);
+    });
 
-        it('throws when given a object', () => {
-            expect(() => normalizeFileExtension({})).toThrow(TypeError)            
-        })
+    it('throws when given a object', () => {
+      assert.throws(() => normalizeFileExtension({}), TypeError);
+    });
 
-        it('throws when given null', () => {
-            expect(() => normalizeFileExtension(null)).toThrow(TypeError)            
-        })
+    it('throws when given null', () => {
+      assert.throws(() => normalizeFileExtension(null), TypeError);
+    });
 
-        it('throws when given undefined', () => {
-            expect(() => normalizeFileExtension(undefined)).toThrow(TypeError)            
-        })
+    it('throws when given undefined', () => {
+      assert.throws(() => normalizeFileExtension(undefined), TypeError);
+    });
 
-        it('throws when given a Symbol', () => {
-            expect(() => normalizeFileExtension(Symbol.for('value'))).toThrow(TypeError)            
-        })
-    })
+    it('throws when given a Symbol', () => {
+      assert.throws(
+        () => normalizeFileExtension(Symbol.for('test')),
+        TypeError
+      );
+    });
+  });
 
-    describe('processing', () => {
-        it('returns the given value when it does not begin with a dot', () => {
-            const extensionName= 'html'
-            
-            const normalizedExtension = normalizeFileExtension(extensionName);
+  describe('processing', () => {
+    it('returns the given value when it does not begin with a dot', () => {
+      const extensionName = 'html';
 
-            expect(normalizedExtension).toEqual(extensionName)
-        })
+      const normalizedExtension = normalizeFileExtension(extensionName);
 
-        it('strips the dot from a value when given a dotted extension', () => {
-            const normalizedExtension = normalizeFileExtension('.html');
+      assert.strictEqual(normalizedExtension, extensionName);
+    });
 
-            expect(normalizedExtension).toEqual('html')
-        })
+    it('strips the dot from a value when given a dotted extension', () => {
+      const normalizedExtension = normalizeFileExtension('.html');
 
-        it('does not change values with dots in the non-first position', () => {
-            const extensionName= 'test.js'
-            
-            const normalizedExtension = normalizeFileExtension(extensionName);
+      assert.strictEqual(normalizedExtension, 'html');
+    });
 
-            expect(normalizedExtension).toEqual(extensionName)
-        })
+    it('does not change values with dots in the non-first position', () => {
+      const extensionName = 'test.js';
 
-        it('removes only the first dot from values with dots in the non-first position', () => {            
-            const normalizedExtension = normalizeFileExtension('.test.js');
+      const normalizedExtension = normalizeFileExtension(extensionName);
 
-            expect(normalizedExtension).toEqual('test.js')
-        })
-    })
-})
+      assert.strictEqual(normalizedExtension, extensionName);
+    });
+
+    it('removes only the first dot from values with dots in the non-first position', () => {
+      const normalizedExtension = normalizeFileExtension('.test.js');
+
+      assert.strictEqual(normalizedExtension, 'test.js');
+    });
+  });
+});
